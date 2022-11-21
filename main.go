80b6618515249f6e4b2a03b7cecd8412933b9e2e
Markus Braunwarth <17438264+mbraunwarth@users.noreply.github.com>
2022-11-21 21:53:50 +0100

    [WIP] added netpbm related data structures

--- main.go
+++ main.go
@@ -2,11 +2,28 @@ package main
 
 import "fmt"
 
+type ImageMatrix [][]uint32
+
+type Netpbm struct {
+	Extension   string
+	MagicNumber uint8
+	Width       uint32
+	Height      uint32
+	Matrix      ImageMatrix
+}
+
+// PBM portable bitmap format. Only containing black or white pixel. Zero represents
+// white, otherwise black.
+type PBM struct {
+	Netpbm
+}
+
 type PPM struct{}
 type PGM struct{}
-type PBM struct{}
 type PNM struct{}
 
 func main() {
-	fmt.Println("vim-go")
+	var m ImageMatrix = make()
+	m[0] = make([]uint32, 0)
+	fmt.Println(m)
 }
